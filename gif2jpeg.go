9406e0e83dea86716592b10999991a5a78084f06
张允虎 <zhangyunhu@frozen.local>
2021-05-24 01:04:27 +0800

    增加http获取

--- gif2jpeg.go
+++ gif2jpeg.go
@@ -6,6 +6,7 @@ import (
 	"image/draw"
 	"image/gif"
 	"image/jpeg"
+	"net/http"
 	"os"
 	"path"
 )
@@ -18,13 +19,47 @@ import (
  * @return err
  */
 
-func TransGif2Jpeg(sourceGif string,frame int)  (filepath string,err error) {
+func TransGif2JpegFile(sourceGif string,frame int)  (filepath string,err error) {
 	gifData, err := os.Open(sourceGif)
 	if err != nil {
 		return "",err
 	}
 	fileName := path.Base(sourceGif)
 	all, err := gif.DecodeAll(gifData)
+	if err != nil {
+		return  "",err
+	}
+	filepath ="/tmp/"
+	filepath += fileName +".jpeg"
+	jp, err := os.Create(filepath)
+
+	if err != nil {
+		return  "",err
+	}
+
+	l := len(all.Image)
+	if l%2 != 0 {
+		l += 1
+	}
+	if frame <= 0 || frame > l {
+		frame = l /2
+	}
+
+	p1 := image.NewPaletted(image.Rect(0, 0, all.Config.Width,all.Config.Height), palette.Plan9)
+	draw.Draw(p1, p1.Bounds(), 	all.Image[frame], image.ZP, draw.Src) //添加图片
+
+	op:= jpeg.Options{90}
+	jpeg.Encode(jp,p1,&op)
+	return filepath,err
+}
+
+func TransGif2JpegUrl(url string,frame int)  (filepath string,err error) {
+	data, err := http.Get(url)
+	if err !=nil{
+		return "",err
+	}
+	fileName := path.Base(url)
+	all, err := gif.DecodeAll(data.Body)
 	if err != nil {
 		return  "",err
 	}
